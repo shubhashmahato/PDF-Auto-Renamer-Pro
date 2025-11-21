da7c01d9a6b21643828fe0c642a8ad3960cdaf38
shubhashmahato <shubhashchandra2022@gmail.com>
2025-11-21 17:03:03 +0300

    Add files via upload

---
+++ PDF Renamer Pro.bat
@@ -0,0 +1,11 @@
+@echo off
+title PDF Auto-Renamer Pro - By Shubhashchandra Mahato
+echo.
+echo   ==================================================
+echo      PDF Auto-Renamer Pro - Starting...
+echo      Developed by Shubhashchandra Mahato (Nepal)
+echo   ==================================================
+echo.
+timeout /t 2 >nul
+start "" "PDF Renamer Pro.html"
+exit
